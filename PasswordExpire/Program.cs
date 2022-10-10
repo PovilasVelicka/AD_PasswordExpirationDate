d8217f98ba14f74c79f0b26d740f3cf11f2ae920
Povilas Velicka <velicka.povilas@gmail.com>
2022-10-10 09:29:24 +0300

    change asyc methods to regular

--- PasswordExpire/Program.cs
+++ PasswordExpire/Program.cs
@@ -29,16 +29,17 @@ namespace PasswordExpire
         {
             Start( );
         }
-        static  async void Start ()
+        static   void Start ()
         {
-            var adUserService = new AdUserService();
+            var adUserService = new AdService();
 
-            SendMessage sendMethods = PrintConsoleMessage;
-            sendMethods += SendSmsMessage;
+            UserDelegate sendMethods = new UserDelegate( PrintConsoleMessage);
+            //sendMethods += SendSmsMessage;
 
 
             // adUserService.SendMessages(sendMethods);
-           var users = await adUserService.GetGroupUsersAsync("LT_All Users");
+            var users = adUserService.Run("LT_All Users", sendMethods);
+
             foreach(var user in users)
             {
                 PrintConsoleMessage(user);
@@ -53,6 +54,8 @@ namespace PasswordExpire
 
         static async void SendSmsMessage (User user)
         {
+            // var expireDate = user.LastPasswordSet?.AddMonths(2).AddDays(10).Date;
+
             var response = await _smsService.SendSmsAsync (user.Mobile, _smsText, "CRAMO");// ("37069553298", _smsText, "CRAMO");//
             SaveLogFile(user.ToString( ));
             SaveLogFile(string.Format("------ Message sent: {0}{1}", response.IsSucess, ", error message: " + response.Message));
